b9f46e408418ea21ed3e86c2aef59a3cab4ea906
yobson <jam.hobson@hotmail.co.uk>
2015-09-04 01:27:57 +0100

    Added GCC support
    Added support for the default compiler to make windows installation
    simpler. May be buggy!

--- command.h
+++ command.h
@@ -12,7 +12,7 @@
 	NSString *_returnType;
 	NSString *_cmdVarName;
 }
-
+/*
 @property (copy) NSString *text; //What is Displayed
 @property (copy) NSString *syntax; //Type of command
 @property (copy) NSString *subSyntax; //Sub type
@@ -22,7 +22,29 @@
 @property (copy) NSString *varName;
 @property (copy) NSString *returnType;
 @property bool attribute;
+*/
 
+//set Methods for GCC
+- (void) setText:(NSString *) set;
+- (void) setSyntax:(NSString *) set;
+- (void) setSubSyntax:(NSString *) set;
+- (void) setIndent:(int) set;
+- (void) setCondition:(NSString *) set;
+- (void) setCmdVarName:(NSString *) set;
+- (void) setVarName:(NSString *) set;
+- (void) setReturnType:(NSString *) set;
+- (void) setAttribute:(bool) set;
+
+//Get Methods for GCC
+- (NSString *) text;
+- (NSString *) syntax;
+- (NSString *) subSyntax;
+- (int) indent;
+- (NSString *) condition;
+- (NSString *) cmdVarName;
+- (NSString *) varName;
+- (NSString *) returnType;
+- (bool) attribute;
 
 
 @end
--- command.m
+++ command.m
@@ -3,7 +3,7 @@
 
 
 @implementation Command
-
+/*
 @synthesize text = _text;
 @synthesize syntax = _syntax;
 @synthesize condition = _condition;
@@ -13,6 +13,76 @@
 @synthesize attribute = _attribute;
 @synthesize returnType = _returnType;
 @synthesize cmdVarName = _cmdVarName;
+*/
 
+// init method for GCC
+- (id) init {
+	_text = [[NSString alloc] init];
+	_syntax = [[NSString alloc] init];
+	_condition = [[NSString alloc] init];
+	_subSyntax = [[NSString alloc] init];
+	_varName = [[NSString alloc] init];
+	_returnType = [[NSString alloc] init];
+	_cmdVarName = [[NSString alloc] init];
+	return self;
+}
+
+//set Methods for GCC
+- (void) setText:(NSString *) set {
+	_text = set;
+}
+- (void) setSyntax:(NSString *) set {
+	_syntax = set;
+}
+- (void) setSubSyntax:(NSString *) set {
+	_subSyntax = set;
+}
+- (void) setIndent:(int) set {
+	_indent = set;
+}
+- (void) setCondition:(NSString *) set {
+	_condition = set;
+}
+- (void) setCmdVarName:(NSString *) set {
+	_cmdVarName = set;
+}
+- (void) setVarName:(NSString *) set {
+	_varName = set;
+}
+- (void) setReturnType:(NSString *) set {
+	_returnType = set;
+}
+- (void) setAttribute:(bool) set {
+	_attribute = set;
+}
+
+//Get Methods for GCC
+- (NSString *) text {
+	return _text;
+}
+- (NSString *) syntax {
+	return _syntax;
+}
+- (NSString *) subSyntax {
+	return _subSyntax;
+}
+- (int) indent {
+	return _indent;
+}
+- (NSString *) condition {
+	return _condition;
+}
+- (NSString *) cmdVarName {
+	return _cmdVarName;
+}
+- (NSString *) varName {
+	return _varName;
+}
+- (NSString *) returnType {
+	return _returnType;
+}
+- (bool) attribute {
+	return _attribute;
+}
 
 @end
